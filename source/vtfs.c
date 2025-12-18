b3a16765d6c1f7aee321f9cbea2a5b6d6685b2d3
lavrentious <lavrent0@yandex.ru>
2025-12-18 23:33:31 +0300

    feat: task 2
    Signed-off-by: lavrentious <lavrent0@yandex.ru>

--- source/vtfs.c
+++ source/vtfs.c
@@ -1,4 +1,8 @@
+#include "vtfs.h"
+
+#include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/mnt_idmapping.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 
@@ -12,11 +16,59 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 
 static int __init vtfs_init(void) {
   LOG("VTFS joined the kernel\n");
-  return 0;
+  int ret = register_filesystem(&vtfs_fs_type);
+  if (ret) {
+    LOG("Failed to register filesystem: %d\n", ret);
+  }
+  return ret;
 }
 
 static void __exit vtfs_exit(void) {
+  unregister_filesystem(&vtfs_fs_type);
   LOG("VTFS left the kernel\n");
+}
+
+struct file_system_type vtfs_fs_type = {
+    .name = "vtfs",
+    .mount = vtfs_mount,
+    .kill_sb = vtfs_kill_sb,
+};
+
+struct dentry* vtfs_mount(
+    struct file_system_type* fs_type, int flags, const char* token, void* data
+) {
+  struct dentry* ret = mount_nodev(fs_type, flags, data, vtfs_fill_super);
+  if (ret == NULL) {
+    printk(KERN_ERR "Can't mount file system");
+  } else {
+    printk(KERN_INFO "Mounted successfully");
+  }
+  return ret;
+}
+
+int vtfs_fill_super(struct super_block* sb, void* data, int silent) {
+  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR, 1000);
+  sb->s_root = d_make_root(inode);
+  if (sb->s_root == NULL) {
+    return -ENOMEM;
+  }
+  printk(KERN_INFO "return 0\n");
+  return 0;
+}
+
+struct inode* vtfs_get_inode(
+    struct super_block* sb, const struct inode* dir, umode_t mode, int i_ino
+) {
+  struct inode* inode = new_inode(sb);
+  if (inode != NULL) {
+    inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
+  }
+  inode->i_ino = i_ino;
+  return inode;
+}
+
+void vtfs_kill_sb(struct super_block* sb) {
+  printk(KERN_INFO "vtfs super block is destroyed. Unmount successfully.\n");
 }
 
 module_init(vtfs_init);
---
+++ source/vtfs.h
@@ -0,0 +1,23 @@
+#ifndef _VTFS_H
+#define _VTFS_H
+
+#include <linux/fs.h>
+
+#define MODULE_NAME "vtfs"
+#define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
+
+extern struct file_system_type vtfs_fs_type;
+
+struct dentry* vtfs_mount(
+    struct file_system_type* fs_type, int flags, const char* token, void* data
+);
+
+int vtfs_fill_super(struct super_block* sb, void* data, int silent);
+
+void vtfs_kill_sb(struct super_block* sb);
+
+struct inode* vtfs_get_inode(
+    struct super_block* sb, const struct inode* dir, umode_t mode, int i_ino
+);
+
+#endif
