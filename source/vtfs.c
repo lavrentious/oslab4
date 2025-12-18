e83770cf69d2aec578bd1ea9d269b3ae80c106eb
lavrentious <lavrent0@yandex.ru>
2025-12-18 23:59:52 +0300

    feat: task 4
    Signed-off-by: lavrentious <lavrent0@yandex.ru>

--- source/vtfs.c
+++ source/vtfs.c
@@ -90,6 +90,15 @@ void vtfs_kill_sb(struct super_block* sb) {
 struct dentry* vtfs_lookup(
     struct inode* parent_inode, struct dentry* child_dentry, unsigned int flag
 ) {
+  ino_t root = parent_inode->i_ino;
+  const char* name = child_dentry->d_name.name;
+  if (root == 100 && !strcmp(name, "test.txt")) {
+    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFREG, 101);
+    d_add(child_dentry, inode);
+  } else if (root == 100 && !strcmp(name, "dir")) {
+    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFDIR, 200);
+    d_add(child_dentry, inode);
+  }
   return NULL;
 }
 
